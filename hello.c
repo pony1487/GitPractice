f7d81e2486fdfb489d4d125c7658c8d35e1c83f1
Ronan <connollyro@gmail.com>
2018-05-27 16:29:37 +0100

    Added add_numbers function

--- hello.c
+++ hello.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+int add_numbers(int a, int b);
+
 int main()
 {
+	int result = 0;
 
 	printf("Hello World: This is the dev branch\n");
+
+	result = add_numbers(5,5);
+	printf("%d\n", result);
+
 	return 0;
+}
+
+
+int add_numbers(int a, int b)
+{
+	int sum = 0;
+	sum = a + b;
+	return sum;
 }
