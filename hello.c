57642879707e01bd7762081ce9c5d7e9aa1178cb
Ronan <connollyro@gmail.com>
2018-05-27 16:32:04 +0100

    Subtract function added

--- hello.c
+++ hello.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+int subtract_numbers(int a,int b);
+
 int main()
 {
+	int result = 0;
 
 	printf("Hello World: This is the master and  dev branch combined\n");
+
+	result = subtract_numbers(8,2);
+	printf("%d\n", result);
 	return 0;
+}
+
+int subtract_numbers(int a, int b)
+{
+
+	return a - b;
 }
